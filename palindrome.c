8fa6005f03407837e75818abe08275a78505efdc
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-02-13 14:32:34 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rev=0,remain,org=0;
+printf("Enter the integer");
+scanf("%d", &n);
+org=n;
+while(n!=0)
+{
+remain=n%10;
+rev=rev*10+remain;
+n/=10;
+}
+if(org==rev)
+{
+printf("palindrome.");
+}
+else
+{
+printf(" not a palindrome.");
+}
+getch();
+}
